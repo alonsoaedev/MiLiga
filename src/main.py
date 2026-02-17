f480ce5083bc413625df02e3fa7dec587b1329da
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-16 20:45:21 -0600

    Remove useless code

--- src/main.py
+++ src/main.py
@@ -4,12 +4,9 @@
 from fastapi import FastAPI
 
 # own
-from app.controllers.tournament import router as tournament_router
 
 app = FastAPI()
 
 @app.get("/api/v1/health")
 async def health():
     return {"status": "ok"}
-
-app.include_router(tournament_router)
